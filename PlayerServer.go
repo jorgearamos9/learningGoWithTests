964b98796867c2a10765f6c03dd895e9d610b6d6
Jorge Ramos <jorgearamos9@gmail.com>
2019-12-21 17:07:29 -0500

    Add the PlayerStore mvp implementation

--- PlayerServer.go
+++ PlayerServer.go
@@ -6,21 +6,17 @@ import (
 	"strings"
 )
 
+type PlayerStore interface {
+    GetPlayerScore(name string) int
+} 
+
 // PlayerServer implemenents the http server logic 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	fmt.Printf("Got a request with URL %s, for player: %s\n",r.URL.Path,player)
-	fmt.Fprint(w, GetPlayerScore(player))
+type PlayerServer struct {
+    store PlayerStore
 }
 
-func GetPlayerScore(name string) string {
-    if name == "Pepper" {
-        return "20"
-    }
-
-    if name == "Floyd" {
-        return "10"
-    }
-
-    return ""
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	fmt.Printf("Got a request with URL %s, for player: %s\n",r.URL.Path,player)
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
 }
--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
     "log"
     "net/http"
+    "fmt"
 )
 
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+    fmt.Printf("Inside GetPlayerScore, name received: %s\n", name)
+    return 123
+}
+
 func main() {
-    handler := http.HandlerFunc(PlayerServer)
-    if err := http.ListenAndServe(":5000", handler); err != nil {
+    server := &PlayerServer{&InMemoryPlayerStore{}}
+    if err := http.ListenAndServe(":5000", server); err != nil {
         log.Fatalf("could not listen on port 5000 %v", err)
     }
 }
