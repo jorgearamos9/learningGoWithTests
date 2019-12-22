64a300605a0799989561f20fccafd25138be7dd0
Jorge Ramos <jorgearamos9@gmail.com>
2019-12-22 16:16:48 -0500

    Add implementation for POST wins

--- PlayerServer.go
+++ PlayerServer.go
@@ -16,14 +16,27 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	fmt.Printf("Got a request with URL %s, for player: %s\n",r.URL.Path,player)
-	
-	score := p.store.GetPlayerScore(player)
+	switch r.Method {
+    case http.MethodPost:
+        p.processWin(w)
+    case http.MethodGet:
+        p.showScore(w, r)
+    }
+
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
+    player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+    score := p.store.GetPlayerScore(player)
 
     if score == 0 {
         w.WriteHeader(http.StatusNotFound)
     }
 
     fmt.Fprint(w, score)
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter) {
+    w.WriteHeader(http.StatusAccepted)
 }
