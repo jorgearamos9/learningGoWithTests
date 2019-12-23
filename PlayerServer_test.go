ac641f1eb323cc7c6fa76c394b44e3502c325083
Jorge Ramos <jorgearamos9@gmail.com>
2019-12-23 15:27:02 -0500

    Add integration test - this will fail

--- PlayerServer_test.go
+++ PlayerServer_test.go
@@ -68,6 +68,22 @@ func TestStoreWins(t *testing.T) {
 
 	})
 }
+// Integration test
+func TestRecordingWinsAndRetrievingThem(t *testing.T) {
+    store := InMemoryPlayerStore{}
+    server := PlayerServer{&store}
+    player := "Pepper"
+
+    server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+    server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+    server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+
+    response := httptest.NewRecorder()
+    server.ServeHTTP(response, newGetScoreRequest(player))
+    assertStatus(t, response.Code, http.StatusOK)
+
+    assertResponseBody(t, response.Body.String(), "3")
+}
 
 type StubPlayerStore struct {
 	scores   map[string]int
