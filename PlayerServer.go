b029448e81e83dd0b685f63f300722eaaf23a842
Jorge Ramos <jorgearamos9@gmail.com>
2019-12-23 20:57:23 -0500

    Refactor the router implementation, dry it up

--- PlayerServer.go
+++ PlayerServer.go
@@ -17,19 +17,22 @@ type PlayerServer struct {
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	router := http.NewServeMux()
-	router.Handle("/league", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.WriteHeader(http.StatusOK)
-	}))
-	router.Handle("/players/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		player := r.URL.Path[len("/players/"):]
+	router.Handle("/league", http.HandlerFunc(p.legueHandle))
+	router.Handle("/players/", http.HandlerFunc(p.playerHandle))
+	router.ServeHTTP(w, r)
+}
+
+func (p *PlayerServer) legueHandle(w http.ResponseWriter, r *http.Request){
+	w.WriteHeader(http.StatusOK)
+}
+func (p *PlayerServer) playerHandle(w http.ResponseWriter, r *http.Request){
+	player := r.URL.Path[len("/players/"):]
 		switch r.Method {
 		case http.MethodPost:
 			p.processWin(w, player)
 		case http.MethodGet:
 			p.showScore(w, player)
 		}
-	}))
-	router.ServeHTTP(w, r)
 }
 
 func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
