a82eae70fba57a944da439b551122b12dfb7234e
Jorge Ramos <jorgearamos9@gmail.com>
2019-12-29 22:27:25 -0500

    Implements sorted League

--- FileSystemPlayerStore_test.go
+++ FileSystemPlayerStore_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestFileSystemStore(t *testing.T) {
 
-	t.Run("/league from a reader", func(t *testing.T) {
+	t.Run("/league from a reader: league sorted", func(t *testing.T) {
 		database, cleanDatabase := createTempFile(t, `[
             {"Name": "Cleo", "Wins": 10},
             {"Name": "Chris", "Wins": 33}]`)
@@ -15,8 +15,8 @@ func TestFileSystemStore(t *testing.T) {
 		got := store.GetLeague()
 
 		want := []Player{
-			{"Cleo", 10},
 			{"Chris", 33},
+			{"Cleo", 10},
 		}
 
 		assertLeague(t, got, want)
