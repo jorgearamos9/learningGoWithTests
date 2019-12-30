6f246a2c3ff230bed345eb904d16e5f0726edd84
Jorge Ramos <jorgearamos9@gmail.com>
2019-12-29 20:23:03 -0500

    Refactor: create multiple test files

---
+++ league_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"testing"
+	"net/http"
+	"net/http/httptest"
+)
+
+const jsonContentType = "application/json"
+
+func TestLeague(t *testing.T) {
+
+	t.Run("it returns 200 on /league", func(t *testing.T) {
+		wantedLeague := []Player{
+			{"Cleo", 32},
+			{"Chris", 20},
+			{"Tiest", 14},
+		}
+
+		store := StubPlayerStore{nil, nil, wantedLeague}
+		server := NewPlayerServer(&store)
+
+		request := newLeagueRequest()
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		got := getLeagueFromResponse(t, response.Body)
+		assertStatus(t, response.Code, http.StatusOK)
+		assertLeague(t, got, wantedLeague)
+		assertContentType(t, response, jsonContentType)
+	})
+}
